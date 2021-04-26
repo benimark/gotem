654ccd629db5143c8c5e6e9f386dade290394bb0
Béni Márk <benimark71@gmail.com>
2021-04-26 21:35:27 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.get('/',(req,res)=>{
 
 if(process.env.NODE_ENV === 'production'){
     console.log("Bro");
-    app.use(express.static(path.join(__dirname,'/frontend/build')))
+    app.use(express.static(path.join(__dirname,'frontend/build')))
 
     app.get('*',(req,res)=>{
         res.sendFile(path.join(__dirname,'frontend','build','index.html'))
